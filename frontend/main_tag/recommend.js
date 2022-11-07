68c3c5eb4fa449b913e4b3122ea9fc368fc220d6
jihyun-cho-0 <roseview808@gmail.com>
2022-11-07 12:03:55 +0900

    feat : recommend view(ing)

--- frontend/main_tag/recommend.js
+++ frontend/main_tag/recommend.js
@@ -1,4 +1,3 @@
-// 로딩이 완료되면 실행  | console.log()는 python의 print() 역할과 같음
 window.onload = ()=>{
     console.log("recommend.js 로딩완료");
     recommend_info_fuc()
@@ -7,16 +6,19 @@ window.onload = ()=>{
 
 // 추천곡들의 정보를 쿠카에서 불러오는 함수
 async function recommend_info_fuc() {
-    // 쿠키에서 정보 불러오기
-    // 정보 추출, html 요소로 만들어주기
-    // url 정보에서 크롤링 한 뒤 앨범 이미지 가져와서 넣어주기
-    // url 주소로 앨범커버랑 음원 링크 크롤링
+  
     var music_data = JSON.parse(localStorage.getItem('tempdata'));
     var music = document.getElementById("music");
     for (i=0; i < music_data.length; i++) {
     console.log('가수 :', music_data[i]['singer'], '제목 :', music_data[i]['title'], 'url :', music_data[i]['url'])
-    // url로 크롤링 1. 앨범커버 2. 음원링크
-    // 가져온 데이터 리스트를 돌면서 div 만들기
+    
+
+    // 곡 좋아요
+    const music_like = document.createElement("input"); // div 생성 (아티스트)
+    music_like.setAttribute("type","checkbox"); // clss 지정 (아티스트)
+    music_like.className = "music_like" + i;
+    // music_like.setAttribute("id",i); // clss 지정 (아티스트)
+    const music_likes = music.appendChild(music_like) // all_tags 안에 tag 추가 (아티스트)
     
     // 음악 제목 
     const music_title = document.createElement("div"); // div 생성 (제목)
@@ -30,14 +32,15 @@ async function recommend_info_fuc() {
     music_singer.setAttribute("class","music_singer"); // clss 지정 (아티스트)
     music_singer.innerText = music_data[i]['singer']; // div 내용 텍스트 (아티스트) 
     const music_singers = music.appendChild(music_singer) // all_tags 안에 tag 추가 (아티스트)
+    var p = document.createElement("p");
 
 
-    // 유튜브 링크
-    const music_url = document.createElement("iframe"); // div 생성 (유튜브링크)
-    music_url.setAttribute("class","music_url"); // clss 지정 (유튜브링크)
-    music_url.setAttribute("src",music_data[i]['url']); // clss 지정 (유튜브링크)
-    music_url.innerText = music_data[i]['url']; // div 내용 텍스트 (유튜브링크) /// 나중에 삭제 확인용 코드
-    const music_urls = music.appendChild(music_url) // all_tags 안에 tag 추가 (유튜브링크)
+    // // 곡 상세페이지로 이동하는 버튼
+    // const music_info = document.createElement("div"); // div 생성 (유튜브링크)
+    // music_info.setAttribute("onclick","location.href="+music_data[i]['url']);
+    // music_info.setAttribute("class","music_info"); // clss 지정 (유튜브링크) -> 상세페이지로 넘어가도록
+    // music_info.innerText = "곡정보확인"; // div 내용 텍스트 (유튜브링크) /// 나중에 삭제 확인용 코드
+    // const music_infos = music.appendChild(music_info) // all_tags 안에 tag 추가 (유튜브링크)
 
 
     }
